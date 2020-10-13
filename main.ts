5749ab93ee54f17c98c2456a7fccdfec66b4ca24
Internal exception: java.lang.NoClassDefFoundError <dilanolivera12@gmail.com>
2020-10-13 12:55:18 -0300

    More clean i guess.
    Added the function to pass from X 0 to X 4 when you press the "A" button.
    Added the function to pass from X 4 to X 0 when you press the "B" button.

--- main.ts
+++ main.ts
@@ -1,26 +1,14 @@
-let points = 0;
-let spr = game.createSprite(randint(0, 4), 0);
-let sprG = game.createSprite(2, 4);
-let finished = false;
-basic.forever(function () {
-    spr.turnRight(90)
-    while (true) {
-        if (!finished) {
-            basic.pause((points > 5) ? (points > 10) ? 250 : 500: 1000)
-            spr.move(1)
-            if (spr.get(LedSpriteProperty.Y) == 4) {
-                if (!sprG.isTouching(spr)) {
-                    notTouching()
-                } else {
-                    touching();
-                }
-            }
-        } else {
-            basic.showNumber(points)
-        }
+function tryReset () {
+    if (!(finished)) {
+        return
     }
-})
-
+    finished = false
+    spr = game.createSprite(randint(0, 4), 0)
+    sprG = game.createSprite(2, 4)
+    spr.turnRight(90)
+points = 0
+    basic.clearScreen()
+}
 input.onButtonPressed(Button.A, function () {
     tryReset()
     if (sprG.get(LedSpriteProperty.X) == 0) {
@@ -28,7 +16,13 @@ input.onButtonPressed(Button.A, function () {
     } else {
         sprG.move(-1)
     }
-});
+})
+function notTouching () {
+    spr.delete()
+    spr = game.createSprite(randint(0, 4), 0)
+    spr.turnRight(90)
+points += 1
+}
 input.onButtonPressed(Button.B, function () {
     tryReset()
     if (sprG.get(LedSpriteProperty.X) == 4) {
@@ -36,27 +30,34 @@ input.onButtonPressed(Button.B, function () {
     } else {
         sprG.move(1)
     }
-});
-
-function tryReset() {
-    if (!finished) return;
-    finished = false;
-    spr = game.createSprite(randint(0, 4), 0)
-    sprG = game.createSprite(2, 4)
-    spr.turnRight(90)
-    points = 0
-}
-
-function notTouching () {
-    spr.delete()
-    spr = game.createSprite(randint(0, 4), 0)
-    spr.turnRight(90)
-    points++
-}
-
-function touching() {
+})
+function touching () {
     spr.delete()
     sprG.delete()
     basic.showNumber(points)
     finished = true
 }
+let finished = false
+let sprG: game.LedSprite = null
+let spr: game.LedSprite = null
+let points = 0
+spr = game.createSprite(randint(0, 4), 0)
+sprG = game.createSprite(2, 4)
+basic.forever(function () {
+    spr.turnRight(90)
+while (true) {
+        if (!(finished)) {
+            basic.pause((points > 5) ? (points > 10) ? (points > 15) ? 100 : 250 : 500 : 1000)
+            spr.move(1)
+            if (spr.get(LedSpriteProperty.Y) == 4) {
+                if (!(sprG.isTouching(spr))) {
+                    notTouching()
+                } else {
+                    touching()
+                }
+            }
+        } else {
+            basic.showNumber(points)
+        }
+    }
+})
