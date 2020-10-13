8b4373646791be0284624976936f23d90e4919a1
Internal exception: java.lang.NoClassDefFoundError <dilanolivera12@gmail.com>
2020-10-13 12:36:02 -0300

    Fixes.
    Maked the player can revive when press a or b.
    Changed the if.

--- main.ts
+++ main.ts
@@ -1,29 +1,45 @@
 let points = 0;
 let spr = game.createSprite(randint(0, 4), 0);
-let sprG = game.createSprite(0, 4);
+let sprG = game.createSprite(2, 4);
+let finished = false;
 basic.forever(function () {
     spr.turnRight(90)
     while (true) {
-        basic.pause(1000)
-        spr.move(1)
-        if (spr.get(LedSpriteProperty.Y) == 4) {
-            if (sprG.isTouching(spr)) {
-                spr.delete();
-                spr = game.createSprite(randint(0, 4), 0);
-                spr.turnRight(90)
-                points++;
-            } else {
-                spr.delete();
-                sprG.delete();
-                basic.showNumber(points)
+        if (!finished) {
+            basic.pause(1000)
+            spr.move(1)
+            if (spr.get(LedSpriteProperty.Y) == 4) {
+                if (!sprG.isTouching(spr)) {
+                    spr.delete();
+                    spr = game.createSprite(randint(0, 4), 0);
+                    spr.turnRight(90)
+                    points++;
+                } else {
+                    spr.delete();
+                    sprG.delete();
+                    basic.showNumber(points)
+                    finished = true;
+                }
             }
+        } else {
+            basic.showNumber(points)
         }
     }
 })
 
 input.onButtonPressed(Button.A, function () {
+    tryReset();
     sprG.move(-1);
 });
 input.onButtonPressed(Button.B, function () {
+    tryReset();
     sprG.move(1);
 });
+
+function tryReset() {
+    if (!finished) return;
+    finished = false;
+    spr = game.createSprite(randint(0, 4), 0);
+    sprG = game.createSprite(2, 4);
+    spr.turnRight(90)
+}
